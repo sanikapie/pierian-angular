f422cdd7fdf1c16707515e0a448b8cc26a01a7f6
sanikapie <sanika.p.pierian@gmail.com>
2019-08-05 14:30:19 +0530

    hello

---
+++ ts/type.ts
@@ -0,0 +1,19 @@
+
+let nm = 'android'
+// nm = 10
+
+let num: number = 10
+let isCv: boolean = false
+let weight: number = 456.9
+let obj: { [key: string]: boolean } = {
+    age: 7 == 7,
+    cb: true
+}
+let fn: (n: number) => boolean = function (n: number) {
+    return true
+}
+
+let mixed: number | string | {} | (() => void) = 'kjhkj'
+
+let op: any = ''
+
